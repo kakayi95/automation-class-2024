d8934582a706c6d60a358cea52a4a1d5934780ca
ashaoshao <vanishao95@gmail.com>
2024-10-19 22:51:00 +0800

    refactor: practice2.py

--- homework/L3/practice2.py
+++ homework/L3/practice2.py
@@ -15,6 +15,7 @@ for responses in responses_new:
 # 200 400 500 201 401 502 404 503
 '''
 
+# Version 1
 responses_new = [
     [200, 201],
     [400, 401, 404],
@@ -34,3 +35,20 @@ while True:
     if current_col == last_row_col_index:
         break
     current_col += 1
+    
+# Version 2   
+responses = [
+    [200, 201],
+    [400, 401, 404],
+    [500, 502, 503]
+]
+
+col = 0
+rows = len(responses)
+max_col = len(responses[-1]) - 1
+
+while col <= max_col:
+    for row in range(rows):
+        if col < len(responses[row]):
+            print(responses[row][col], end=' ')
+    col += 1
